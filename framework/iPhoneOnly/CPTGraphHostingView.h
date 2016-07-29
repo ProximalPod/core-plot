99de21bf9e379a9dd4d30c34e5d162a52908fff0
Ryan Holstad <proximalpod@gmail.com>
2016-07-29 10:28:39 -0400

    graph hosting view

--- framework/iPhoneOnly/CPTGraphHostingView.h
+++ framework/iPhoneOnly/CPTGraphHostingView.h
@@ -7,5 +7,6 @@
 @property (nonatomic, readwrite, strong, nullable) CPTGraph *hostedGraph;
 @property (nonatomic, readwrite, assign) BOOL collapsesLayers;
 @property (nonatomic, readwrite, assign) BOOL allowPinchScaling;
-
+@property (nonatomic, readwrite, nullable, cpt_weak_property) UIPinchGestureRecognizer *pinchGestureRecognizer;
+@property (nonatomic, readwrite, nonnull) NSMutableArray<UITouch*> *touchesUp;
 @end
